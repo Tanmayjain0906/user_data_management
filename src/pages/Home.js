9ef98e593f7ab2691e8923a78ce38e1a6dab79da
Tanmayjain0906 <tanmayjain983@gmail.com>
2024-04-20 17:09:16 +0530

    user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div>
+      <div className='btn-container'>
         <button className='home-btn' onClick={() => navigate("/counter")}>Counter</button>
         <button className='home-btn' onClick={() => navigate("/user")}>User Data Form</button>
         <button className='home-btn' onClick={() => navigate("/text-editor")}>Rich Text Editor</button>
+      </div>
+
     </div>
   )
 }
